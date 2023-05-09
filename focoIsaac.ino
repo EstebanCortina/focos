98980e5dea083d202ec3204f3aa667e9544a6b85
IsaacPonce13 <111556869+IsaacPonce13@users.noreply.github.com>
2023-05-09 10:21:32 -0600

    focoIsaac.ino

--- focoIsaac.ino
+++ focoIsaac.ino
@@ -1,23 +1,33 @@
+#define luz 12
 #define sensorMin 0
 #define sensorMax 1024
 #define pin A0
 void setup(){
     Serial.begin(9600);
+    pinMode (luz, OUTPUT); 
 }
+
 void loop(){
-    int sensorReading =analogRead(pin);
-    int range=map(sensorReading, sensorMin, sensorMax, 0, 3);
+    int sensorReading = analogRead(pin);
+    int range = map(sensorReading, sensorMin, sensorMax, 0, 3);
 
     switch(range){
         case 0:
             Serial.println("Peligro fuego");
+            // Encender el foco
+            digitalWrite(luz, LOW);
             break;
         case 1:
             Serial.println("Fuego lejano");
+            // Encender el foco con PWM a una intensidad menor
+            // analogWrite(luz, 64); // 25% de la intensidad máxima (255)
             break;
         case 2:
             Serial.println("No hay fuego");
+            //Foco apagado
+            digitalWrite(luz, HIGH);
             break;
     }
-    dealy(12000);
-}
+    delay(1200);
+} 
+
