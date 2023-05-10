43c33b0ac3353da60fd624c53f33ba7820470716
Esteban Cortina <estebannsebastian@gmail.com>
2023-05-10 14:08:21 -0600

    Final

--- Focos.h
+++ Focos.h
@@ -1,97 +1,59 @@
-// Librerias para sensor de luz ambiental
 #include <Wire.h>
 #include <BH1750.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// Rangos para sensor de Flama
+#define sensorMin 0
+#define sensorMax 4095
 
 // PINES sensor-ESP32
+#define inputHumo 34
+#define inputFlama 35
 
 // PINES ESP32-foco
 #define focoLuxometro 33
+#define focoHumo 25
+#define focoFlama 26
+#define focoMqtt 27
 
 BH1750 lightMeter;
+bool ledState = false;
+
+const char *mqtt_server = "test.mosquitto.org";
+String palabra;
+const int mqtt_port = 1883;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// Configuracion Mqtt
 class Focos
 {
 public:
+  String nombre;
   // Mqtt
-  void callback(char *topic, byte *payload, unsigned int length);
   void mqttSetup();
+  void mqttListen();
   void reconnectMqtt();
-  // Luxometro
+  // bool ledState = false;
+  //  Luxometro
   void luxoSetup();
   void luxoListen();
+  uint16_t lux;
+  // Humo
+  void humoSetup();
+  void humoListen();
+  int valorHumo;
+  // Flama
+  void flamaSetup();
+  void flamaListen();
+  int valorFlama;
+  int range;
 };
-void luxoSetup()
-{
-  Wire.begin();
-  if (lightMeter.begin())
-  {
-    Serial.println("Conectado");
-  }
-  else
-  {
-    Serial.println("No conectado");
-  }
-  pinMode(focoLuxometro, OUTPUT);
-}
-void luxoListen()
-{
-  uint16_t lux = lightMeter.readLightLevel();
-  if (lux > 200)
-  {
-    Serial.print("Luminosidad: ");
-    Serial.print(lux);
-    Serial.println(" lux");
-    digitalWrite(focoLuxometro, LOW);
-  }
-  else
-  {
-    Serial.print("Luminosidad: ");
-    Serial.print(lux);
-    Serial.println(" lux");
-    digitalWrite(focoLuxometro, HIGH);
-  }
-
-  delay(1000);
-}
-/*
-void callback(char *topic, byte *payload, unsigned int length)
-{
-  Serial.print("Mensaje recibido [");
-  Serial.print(topic);
-  Serial.print("] ");
-
-  for (int i = 0; i < length; i++)
-  {
-    Serial.print((char)payload[i]);
-    palabra += (char)payload[i];
-  }
-
-  Serial.println(palabra);
-  palabra = "";
-  ledState = !ledState;
-  digitalWrite(pinLED, ledState);
-}
-
-void mqttSetup()
-{
-  pinMode(pinLED, OUTPUT);
-  // put your setup code here, to run once:
-  // Inicializar la conexión Wi-Fi
-  WiFi.begin("E6CA82", "L21503735312143");
-  Serial.begin(9600);
-
-  // Esperar a que se establezca la conexión
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(1000);
-  }
-
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
-}
 
 void reconnectMqtt()
 {
-  // Loop hasta que estemos conectados
   while (!client.connected())
   {
     Serial.print("Conectando al servidor MQTT...");
@@ -114,4 +76,126 @@ void reconnectMqtt()
     }
   }
 }
-*/
+
+void mqttListen()
+{
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    // Reintentar la conexión
+    WiFi.begin("E6CA82", "L21503735312143");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+      delay(1000);
+    }
+  }
+  if (!client.connected())
+  {
+    reconnectMqtt();
+  }
+  client.loop();
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Mensaje recibido [");
+  Serial.print(topic);
+  Serial.print("] ");
+
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+  }
+  ledState = !ledState;
+  digitalWrite(focoMqtt, ledState);
+}
+
+void mqttSetup()
+{
+  pinMode(focoMqtt, OUTPUT);
+  WiFi.begin("E6CA82", "L215037353121439");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+  }
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void Focos::flamaListen()
+{
+  this->valorFlama = analogRead(inputFlama);
+  this->range = map(this->valorFlama, sensorMin, sensorMax, 0, 4);
+  // Serial.println(sensorReading);
+  switch (this->range)
+  {
+  case 0:
+    Serial.println("Peligro fuego");
+    client.publish("datosFocoEsli", "Peligro Fuego");
+    // Encender el foco
+    digitalWrite(focoFlama, HIGH);
+    break;
+  case 1 || 2:
+    Serial.println("Fuego lejano");
+    client.publish("datosFocoEsli", "Fuego lejano");
+    break;
+  case 3:
+    Serial.println("No hay fuego");
+    client.publish("datosFocoEsli", "No hay fuego");
+    digitalWrite(focoFlama, LOW);
+    break;
+  }
+}
+void Focos::flamaSetup()
+{
+  pinMode(focoFlama, OUTPUT);
+}
+void Focos::humoSetup()
+{
+  pinMode(inputHumo, INPUT);
+  pinMode(focoHumo, OUTPUT);
+}
+void Focos::humoListen()
+{
+  this->valorHumo = analogRead(inputHumo);
+  if (this->valorHumo < 100)
+  {
+    digitalWrite(focoHumo, HIGH);
+    Serial.println("Humo detectado");
+  }
+  else
+  {
+    Serial.println("Humo no detectado");
+    digitalWrite(focoHumo, LOW);
+  }
+}
+void Focos::luxoSetup()
+{
+  Wire.begin();
+  if (lightMeter.begin())
+  {
+    Serial.println("Conectado");
+  }
+  else
+  {
+    Serial.println("No conectado");
+  }
+  pinMode(focoLuxometro, OUTPUT);
+}
+void Focos::luxoListen()
+{
+  this->lux = lightMeter.readLightLevel();
+  if (this->lux > 200)
+  {
+    Serial.print("Luminosidad: ");
+    Serial.print(this->lux);
+    Serial.println(" lux");
+    digitalWrite(focoLuxometro, LOW);
+  }
+  else
+  {
+    Serial.print("Luminosidad: ");
+    Serial.print(this->lux);
+    Serial.println(" lux");
+    digitalWrite(focoLuxometro, HIGH);
+  }
+}
--- sketchPrincipal.ino
+++ sketchPrincipal.ino
@@ -1,1 +1,77 @@
 #include "Focos.h"
+
+Focos f;
+void setup()
+{
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  Serial.println("NUEVO");
+
+  pinMode(27, OUTPUT);
+  WiFi.begin("E6CA82", "L21503735312143");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+  }
+
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  f.luxoSetup();
+  f.humoSetup();
+  f.flamaSetup();
+  // mqttSetup();
+}
+
+void loop()
+{
+  /*
+  if (WiFi.status() != WL_CONNECTED) {
+    // Reintentar la conexión
+    WiFi.begin("E6CA82","L21503735312143");
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(1000);
+    }
+  }
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  // put your main code here, to run repeatedly:
+  */
+  f.luxoListen();
+  delay(100);
+  f.humoListen();
+  delay(100);
+  f.flamaListen();
+  delay(100);
+  mqttListen();
+  delay(1000);
+}
+
+void reconnect()
+{
+  // Loop hasta que estemos conectados
+  while (!client.connected())
+  {
+    Serial.print("Conectando al servidor MQTT...");
+
+    // Intenta conectarse
+    if (client.connect(mqtt_server))
+    {
+      Serial.println("conectado");
+
+      // Suscribirse a un tema
+      client.subscribe("focoEsli");
+    }
+    else
+    {
+      Serial.print("fallo, rc=");
+      Serial.print(client.state());
+      Serial.println(" esperando 5 segundos antes de volver a intentar");
+      // Espera 5 segundos antes de intentar nuevamente
+      delay(5000);
+    }
+  }
+}
