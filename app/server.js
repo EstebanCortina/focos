c92d3d558421681b548a6841d9d4c080d568bfc6
Esteban Cortina <estebannsebastian@gmail.com>
2023-05-10 14:11:14 -0600

    app

--- app/server.js
+++ app/server.js
@@ -1,22 +1,40 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
+const path = require('path');
+const staticPath = path.join(__dirname, "./public");
+app.use(express.static(staticPath));
+
 const mqtt = require('mqtt');
 const client = mqtt.connect('mqtt://test.mosquitto.org');
 
 client.on('connect', () => {
-  console.log("asd");
+  client.subscribe('datosFocoEsli');
 });
 
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+/*
+client.on('message', (topic, message) => {
+  console.log(`Topic[${topic}]: ${message}`);
+});
+*/
+app.get('/flama', (req, res) => {
+  const data = {
+    nombre: "Esteban"
+  }
+  res.render('indexRender', { data })
 })
 
-app.post('/', (req, res) => {
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index3.html');
+})
+
+app.get('/publish', (req, res) => {
   client.publish('focoEsli', 'Hola ESLI');
+  console.log('publish');
   res.status(200);
-  res.redirect('/');
+  res.json("Msj")
 })
 
 
